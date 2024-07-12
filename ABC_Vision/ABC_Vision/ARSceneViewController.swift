1e8112535837c3afecad49f45d41de70e582c42a
Wylan Neely <wylanneely@gmail.com>
2024-07-11 21:46:54 -0700

    updated ARSceneVC
    -fixed bug causing 3D object not to show up

--- ABC_Vision/ABC_Vision/ARSceneViewController.swift
+++ ABC_Vision/ABC_Vision/ARSceneViewController.swift
@@ -23,7 +23,7 @@ class ARSceneViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/Corn2.reality")!
+        let scene = SCNScene(named: "art.scnassets/Gun.scn")!
         
         // Set the scene to the view
         sceneView.scene = scene
