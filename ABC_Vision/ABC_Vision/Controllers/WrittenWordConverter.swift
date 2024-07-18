6025a8e3bf6d0988b9dcdee48d94313d8f9394ac
Wylan Neely <wylanneely@gmail.com>
2024-07-17 17:25:05 -0700

    refactored class name

---
+++ ABC_Vision/ABC_Vision/Controllers/WrittenWordConverter.swift
@@ -0,0 +1,42 @@
+//
+//  WrittenWordToFileConverter.swift
+//  ABC_Vision
+//
+//  Created by Wylan L Neely on 7/15/24.
+//
+
+import Foundation
+
+struct WrittenWordConverter {
+    
+    private let WordToFileDictionary: [String: String] = [
+        "Banana":"Banana",
+        "Camera":"Camera",
+        "Carrot" : "Carrot",
+        "Carrots": "Carrots",
+        "Donut":"Donut",
+        "Earth":"Earth",
+        "Gun":"Gun",
+        "God":"Jesus",
+        "Jesus":"Jesus",
+        "Moon":"Moon",
+        "Orange":"Orange",
+        "Pizza":"Pizza",
+        "President":"President",
+        "Presidents":"Presidents",
+        "Rock":"Rock",
+        "Ship":"Ship",
+        "Sun":"Sun",
+        "Trump":"President",
+        "Orbits":"Orbits",
+        "MoonOrbitsEarth":"MoonOrbitsEarth"
+    ]
+    
+    func getFileNameFrom(_ word: String) -> String {
+       if let filename = WordToFileDictionary[word] {
+            return filename
+       } else {
+           return ""
+       }
+    }
+}
