2b7a216616ba5a8600ca91e50bddb5b65243a5ec
Wylan Neely <wylanneely@gmail.com>
2024-08-12 16:16:28 -0700

    added collection view to shows words

--- ABC_Vision/ABC_Vision/ViewControllers/HomeViewController.swift
+++ ABC_Vision/ABC_Vision/ViewControllers/HomeViewController.swift
@@ -7,17 +7,48 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     var currentPlayer: Player?
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpCollectionView()
+        setPlayerProperties()
     }
     
-
+    func setPlayerProperties(){
+        if let player = currentPlayer {
+            playerNameLabel.text = player.nickname
+        }
+    }
+    
+    @IBOutlet weak var playerNameLabel: UILabel!
+    
+    //MARK: - Collection View
+    @IBOutlet weak var wordStackCollectionView: UICollectionView!
+    
+    func setUpCollectionView(){
+        wordStackCollectionView.delegate = self
+        wordStackCollectionView.dataSource = self
+        let nib = UINib(nibName: "WordStackCollectionCell", bundle: nil)
+        self.wordStackCollectionView.register(nib, forCellWithReuseIdentifier: "WordStackCollectionCell")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = wordStackCollectionView.dequeueReusableCell(withReuseIdentifier: "WordStackCollectionCell", for: indexPath) as? WordStackCollectionCell else {
+            return UICollectionViewCell()
+        }
+            return cell
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
