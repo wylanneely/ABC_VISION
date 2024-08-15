2e61d3ca0182d0daf41fa0f484660a00cd0cfccd
Wylan Neely <wylanneely@gmail.com>
2024-08-15 11:31:41 -0700

    updated selectedState

--- ABC_Vision/ABC_Vision/Views/WordStackCollectionCell.swift
+++ ABC_Vision/ABC_Vision/Views/WordStackCollectionCell.swift
@@ -13,15 +13,33 @@ class WordStackCollectionCell: UICollectionViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.contentView.layer.borderWidth = 1.0
+             self.contentView.layer.borderColor = UIColor.lightGray.cgColor
     }
 
-    @IBOutlet weak var image: UIImageView!
-    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
     
     func setUpWordStackVisuals(wordStack: WordStack?){
         self.wordStack = wordStack
-        
     }
     
+    override var isSelected: Bool {
+          didSet {
+              updateUIForSelectionState()
+          }
+      }
+      
+      private func updateUIForSelectionState() {
+          if isSelected {
+              // Update the cell's appearance for the selected state
+              self.contentView.backgroundColor = .blue
+              titleLabel.textColor = .white
+          } else {
+              // Update the cell's appearance for the normal state
+              self.contentView.backgroundColor = .white
+              titleLabel.textColor = .black
+          }
+      }
+      
 }
