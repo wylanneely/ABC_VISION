957f0705394808aacda19b059b6b8155952b031c
Wylan Neely <wylanneely@gmail.com>
2025-01-04 13:13:13 -0800

    adding support for Russian

---
+++ ABC_Vision/ABC_Vision/Languages/LanguageChecker.swift
@@ -0,0 +1,14 @@
+//
+//  LanguageChecker.swift
+//  ABC_Vision
+//
+//  Created by Wylan L Neely on 1/4/25.
+//
+
+import Foundation
+
+class LanguageChecker {
+    
+    public var language: String = NSLocalizedString("AppLanguage", comment: "for displaying adjusting correct language settings")
+    
+}
