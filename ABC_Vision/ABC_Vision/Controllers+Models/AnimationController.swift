22775d499af45f1047cf91b9a0c15f997067153e
Wylan Neely <wylanneely@gmail.com>
2025-05-28 11:48:44 -0700

    set up for reusability

--- ABC_Vision/ABC_Vision/Controllers+Models/AnimationController.swift
+++ ABC_Vision/ABC_Vision/Controllers+Models/AnimationController.swift
@@ -36,6 +36,19 @@ struct AnimationController {
     
     //MARK: Planets
     
+   
+    
+    func earthRotation(_ node: SCNNode ) {
+        let rotation = CArotation(20)
+        node.addAnimation(rotation, forKey: "rotate")
+        let scale = CAScale()
+        node.addAnimation(scale, forKey: "scale")
+        let zigzag = CAZigZag(position: node.position)
+        node.addAnimation(zigzag, forKey: "zigzag")
+    }
+    
+    //MARK: Helpers
+    
     func CArotation(_ duration: TimeInterval = 15) -> CABasicAnimation  {
         let rotation = CABasicAnimation(keyPath: "rotation")
         rotation.fromValue = SCNVector4(0, 1, 0, 0)
@@ -45,34 +58,31 @@ struct AnimationController {
         return  rotation
     }
     
-    func earthRotation(_ node: SCNNode ) {
-           let rotation = CArotation(20)
-           node.addAnimation(rotation, forKey: "rotate")
-           let scale = CABasicAnimation(keyPath: "scale")
-        
-           scale.fromValue = SCNVector3(1.0, 1.0, 1.0)
-           scale.toValue = SCNVector3(1.2, 1.2, 1.2)
-           scale.duration = 2.0
-           scale.autoreverses = true
-           scale.repeatCount = .infinity
-           node.addAnimation(scale, forKey: "scale")
-
-           let zigzag = CAKeyframeAnimation(keyPath: "position")
-           let currentPosition = node.position
-
-           let offset: Float = 0.05  // About a few inches in AR space
-           zigzag.values = [
-               NSValue(scnVector3: currentPosition),
-               NSValue(scnVector3: SCNVector3(currentPosition.x + offset, currentPosition.y, currentPosition.z)),
-               NSValue(scnVector3: SCNVector3(currentPosition.x, currentPosition.y + offset, currentPosition.z)),
-               NSValue(scnVector3: SCNVector3(currentPosition.x - offset, currentPosition.y, currentPosition.z)),
-               NSValue(scnVector3: SCNVector3(currentPosition.x, currentPosition.y - offset, currentPosition.z)),
-               NSValue(scnVector3: currentPosition)
-           ]
-           zigzag.duration = 30.0
-           zigzag.repeatCount = .infinity
-           node.addAnimation(zigzag, forKey: "zigzag")
+    func CAScale(x:Float = 1.2,y:Float = 1.2,z: Float = 1.2, duration: TimeInterval = 2) -> CABasicAnimation  {
+        let scale = CABasicAnimation(keyPath: "scale")
+        scale.fromValue = SCNVector3(1.0, 1.0, 1.0)
+        scale.toValue = SCNVector3(x,y,z)
+        scale.duration = duration
+        scale.autoreverses = true
+        scale.repeatCount = .infinity
+        return scale
     }
     
-    
+    func CAZigZag(position: SCNVector3 ,offset: Float = 0.05, duration: TimeInterval = 30.0) -> CAKeyframeAnimation  {
+        let zigzag = CAKeyframeAnimation(keyPath: "position")
+        let currentPosition = position
+
+        let offset = offset  // About a few inches in AR space
+        zigzag.values = [
+            NSValue(scnVector3: currentPosition),
+            NSValue(scnVector3: SCNVector3(currentPosition.x + offset, currentPosition.y, currentPosition.z)),
+            NSValue(scnVector3: SCNVector3(currentPosition.x, currentPosition.y + offset, currentPosition.z)),
+            NSValue(scnVector3: SCNVector3(currentPosition.x - offset, currentPosition.y, currentPosition.z)),
+            NSValue(scnVector3: SCNVector3(currentPosition.x, currentPosition.y - offset, currentPosition.z)),
+            NSValue(scnVector3: currentPosition)
+        ]
+        zigzag.duration = duration
+        zigzag.repeatCount = .infinity
+        return zigzag
+    }
 }
